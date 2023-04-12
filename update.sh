b74558438f65d1941a21d3bacf58405eca5a18e1
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2023-04-12 19:24:45 -0300

    fix: and logic

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ git config user.email github-actions@github.com
 for branch in ${branches[@]}; do
   echo "Working on $branch"
   git checkout $branch
-  git merge --no-commit --no-ff main err=$? || err=$?
+  git merge --no-commit --no-ff main && err=$? || err=$?
   git merge --abort
   if [ $err -eq 1 ] ; then
     echo "❌ Merge with conflicts. Resolve and merge manually"

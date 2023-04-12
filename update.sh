58cac8da83945aa7ef092d0a56a3365de8afea85
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2023-04-12 16:15:22 -0300

    cria action

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -l
+set -e
+
+branchs=['rubric']
+
+git config user.name "Update branch"
+git config user.email github-actions@github.com
+
+for branch in $branchs; do
+  echo "Working on $branch"
+  git checkout $branch
+  git merge --no-commit --no-ff main ||
+  conflicts=$?
+  if [ "$conflicts" -eq "1" ] ; then
+    git merge --abort
+    echo "❌ Merge with conflicts. Resolve and merge manually"
+  else
+    echo "Merge without conflicts"
+    git merge --abort
+    git merge main --no-edit -m "Merged by auto-update-rubric-pr-action"
+    git push origin $branch
+    echo "✅ Branch '$branch' was updated successfully"
+  fi
+done
